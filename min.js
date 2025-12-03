e07f20c67816f4a60010438fec6c15b4e545a33e
hai07t <hai07t@gmail.com>
2025-12-03 13:23:32 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,10 +1,10 @@
-const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+// const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-if (
-  userTimeZone === "Asia/Ho_Chi_Minh" || 
-  userTimeZone === "Asia/Saigon"
-) {
-    window.location.href = "https://www.google.com/";
-} else if (userTimeZone === "Asia/Bangkok") {
-    window.location.href = "https://www.google.com/";
-}
+// if (
+//   userTimeZone === "Asia/Ho_Chi_Minh" || 
+//   userTimeZone === "Asia/Saigon"
+// ) {
+//     window.location.href = "https://www.google.com/";
+// } else if (userTimeZone === "Asia/Bangkok") {
+//     window.location.href = "https://www.google.com/";
+// }

